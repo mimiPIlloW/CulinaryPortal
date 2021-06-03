03e6f23560a4fd7cd3a73b736140c4d37036d0c2
mimiPIllow <72455183+mimiPIlloW@users.noreply.github.com>
2021-06-03 14:04:50 +0300

    edit errors and add user pages and delete them

--- src/main/java/com/itportal/culinary/portal/controllers/UserController.java
+++ src/main/java/com/itportal/culinary/portal/controllers/UserController.java
@@ -1,10 +1,15 @@
 package com.itportal.culinary.portal.controllers;
 
+import com.itportal.culinary.portal.entity.Breakfast;
+import com.itportal.culinary.portal.entity.User;
 import com.itportal.culinary.portal.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,6 +22,12 @@ public class UserController {
     public String userList(Model model) {
         model.addAttribute("users", userRepository.findAll());
         return "userList";
+    }
+    @PostMapping("/user/{id}/delete")
+    public String deleteUserId(@PathVariable(name = "id") long id, Model model) {
+        User user = userRepository.findById(id).orElseThrow();
+        userRepository.delete(user);
+        return "redirect:/user";
     }
 }
 
